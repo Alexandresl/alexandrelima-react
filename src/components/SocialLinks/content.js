fa5023ab4082c49fca15ca624ab24215aa113ca5
Alexandre Lima <limaalexguitar@gmail.com>
2019-10-11 17:15:04 -0300

    SocialLinks Parte 1

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,14 @@
+const links = [
+  {
+    label: "Youtube",
+    url: "https://www.youtube.com/channel/UCULzSTxbJ4TmzjwcuAnzi_A",
+  },
+  {
+    label: "Twitter",
+    url: "https://twitter.com/lexandresl",
+  },
+  {
+    label: "Github",
+    url: "https://github.com/Alexandresl",
+  },
+]
