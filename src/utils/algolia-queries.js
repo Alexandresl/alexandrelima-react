050f120c5cd550c8cb37a6feec4324cae836c91c
Alexandre Lima <limaalexguitar@gmail.com>
2019-10-12 20:06:53 -0300

    Finalizando Search

--- src/utils/algolia-queries.js
+++ src/utils/algolia-queries.js
@@ -9,17 +9,18 @@ const postsQuery = `{
             slug
           }
           frontmatter {
+            title
+            background
             category
             date_timestamp: date
             date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
             description
-            title
           }
           excerpt(pruneLength: 5000)
         }
       }
     }
-  }`
+}`
 
 const flatten = arr =>
   arr.map(({ node: { frontmatter, ...rest } }) => ({
