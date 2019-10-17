4e26e44cb22e79dd51ad2edd02a33dc3e7c9effa
Alexandre Lima <limaalexguitar@gmail.com>
2019-10-17 20:51:06 -0300

    limpo

--- src/utils/getThemeColor.js
+++
@@ -1,8 +0,0 @@
-const getThemeColor = () => {
-  const theme = typeof window !== "undefined" && window.__them
-
-  if (theme === "light") return "#f0f0f3"
-  if (theme === "dark") return "#3B3C40"
-}
-
-export default getThemeColor
