94b5f1f250b72a1db57b25874191b3d01332844d
Alexandre Lima <limaalexguitar@gmail.com>
2019-10-22 23:17:32 -0300

    atualizacao getThemeColor

--- src/utils/getThemeColor.js
+++ src/utils/getThemeColor.js
@@ -1,6 +1,5 @@
 const getThemeColor = () => {
-  const theme = typeof window !== "undefined" && window.__them
-  console.log(theme)
+  const theme = typeof window !== "undefined" && window.__theme
 
   if (theme === "light") return "#f0f0f3"
   if (theme === "dark") return "#3B3C40"
