4911372ef9d8f68778874573f920e8dd74862a7a
Alexandre Lima <limaalexguitar@gmail.com>
2019-12-15 18:01:02 -0300

    avatar

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -9,7 +9,7 @@ const Avatar = () => {
       query {
         avatarImage: file(relativePath: { eq: "profile3.jpg" }) {
           childImageSharp {
-            fluid(maxWidth: 126) {
+            fluid(maxWidth: 252) {
               ...GatsbyImageSharpFluid
             }
           }
