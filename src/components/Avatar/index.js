343a0978fe62e0fd1a9c83e26fc0d241d4267fff
Alexandre Lima <limaalexguitar@gmail.com>
2019-12-15 17:45:54 -0300

    corrigindo imagem

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -9,7 +9,7 @@ const Avatar = () => {
       query {
         avatarImage: file(relativePath: { eq: "profile3.jpg" }) {
           childImageSharp {
-            fluid(maxWidth: 100) {
+            fluid(maxWidth: 60) {
               ...GatsbyImageSharpFluid
             }
           }
